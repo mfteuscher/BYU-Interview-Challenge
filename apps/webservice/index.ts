07aa679db88a6600ac0fa1e3901cd283099eb49b
Michael Teuscher <70554955+mfteuscher@users.noreply.github.com>
2024-01-30 20:17:15 -0700

    limit api return to 10, cleanup

--- apps/webservice/index.ts
+++ apps/webservice/index.ts
@@ -1,23 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+const MOVIE_DB_ENDPOINT = 'https://api.themoviedb.org/3/search/movie';
 
 const app = express();
 app.use(cors());
 
 const port = parseInt(process.env.PORT || '4000');
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
-
-const MOVIE_DB_ENDPOINT = 'https://api.themoviedb.org/3/search/movie';
-
 app.get('/movies', async (req, res) => {
-	console.log('endpoint hit');
-	// console.log query params
 	const { search } = req.query;
-
-	console.log(search);
 
 	const response = await fetch(
 		`${MOVIE_DB_ENDPOINT}?query=${search}&language=en-US`,
@@ -30,16 +21,22 @@ app.get('/movies', async (req, res) => {
 	);
 
 	if (!response.ok) {
-	    res.status(500).send('Something went wrong with The Movie Database. Please try again later.');
-	    return;
+		res
+			.status(500)
+			.send(
+				'Something went wrong with The Movie Database. Please try again later.'
+			);
+		return;
 	}
 
 	const movies: any = await response.json();
 
-	const responseBody = movies.results.map((movie: any) => ({
+	const responseBody = movies.results.slice(0,10).map((movie: any) => ({
 		movie_id: movie.id,
 		title: movie.original_title,
-		poster_image_url: movie.poster_path ? `https://image.tmdb.org/t/p/original${movie.poster_path}` : 'https://placehold.co/600x900?text=No%20Poster%20Avaliable',
+		poster_image_url: movie.poster_path
+			? `https://image.tmdb.org/t/p/original${movie.poster_path}`
+			: 'https://placehold.co/600x900?text=No%20Poster%20Avaliable',
 		popularity_summary: `${movie.popularity} out of ${movie.vote_count}`,
 	}));
 
